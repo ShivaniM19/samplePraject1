fd85b9d7e3fdd6ea777a425b60ae42a2157c3fdb
ShivaniMane19 <maneshivani2001@gmail.com>
2024-10-16 12:04:38 +0530

    Greatest of two numbwr

---
+++ Greatest.java
@@ -0,0 +1,12 @@
+public class Greatest {
+    public static void main(String[] args) {
+        int a=13;
+        int b=78;
+        if (a>b) {
+            System.out.println("a is greatest");
+        }
+        else{
+            System.out.println("b is greatest");
+        }
+    }
+}
